bdfea621ad3505c061b87073dd6530d5d565fc64
Andy Tao <mrtaoyx@gmail.com>
2026-09-08 18:52:35 +0800

    chore: update zotero-pdf2zh-pro to v1.6.5

--- Formula/zotero-pdf2zh-pro.rb
+++ Formula/zotero-pdf2zh-pro.rb
@@ -1,8 +1,8 @@
 class ZoteroPdf2zhPro < Formula
   desc "Local PDF translation server for Zotero PDF2ZH Pro"
   homepage "https://github.com/study-233/zotero-pdf2zh-pro"
-  url "https://github.com/study-233/zotero-pdf2zh-pro.git", using: :git, revision: "2f702bb2ad6b673c126ef134e89d4dbd05aa3810"
-  version "1.6.4"
+  url "https://github.com/study-233/zotero-pdf2zh-pro.git", using: :git, revision: "cda712895f90be12d33af064e154f485898e49b7"
+  version "1.6.5"
   license "AGPL-3.0-or-later"
 
   depends_on "uv" => :build
